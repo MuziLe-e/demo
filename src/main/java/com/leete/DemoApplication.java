6ddbc9c23072363a38b1d2b731117fcd2aa4ec5f
latte <lattesu@126.com>
2019-03-08 18:04:00 +0800

    增加多环境配置文件切换

--- src/main/java/com/leete/DemoApplication.java
+++ src/main/java/com/leete/DemoApplication.java
@@ -1,11 +1,14 @@
 package com.leete;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 @MapperScan(basePackages="com.leete.mapper")
 public class DemoApplication {
 
